70e55d4fc37a30e0567a5396bc07a5e2fd1015c9
EtanoloYT <etanoloyt@gmail.com>
2025-02-10 19:33:06 +0100

    BACK-32 Updated edit

--- app/controllers/construction_sites_controller.ts
+++ app/controllers/construction_sites_controller.ts
@@ -78,9 +78,13 @@ export default class ConstructionSitesController {
    * @param params: id of the construction site, request: construction site data to update
    * @returns the updated construction site
    */
-  update({ params, request }: HttpContext) {
-    let constructionSite = ConstructionSite.findByIdAndUpdate(params.id, request.all())
-    return constructionSite
+  async update({ params, request, response }: HttpContext) {
+    try {
+      let constructionSite = await ConstructionSite.findByIdAndUpdate(params.id, request.all())
+      return response.ok(constructionSite)
+    } catch (error) {
+      return response.notFound()
+    }
   }
 
   /**
